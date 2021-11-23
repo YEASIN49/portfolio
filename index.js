8dbe1b0f1de2f283179473e910b9128a894a3ece
MD.YEASIN ALI <myeasin49@gmail.com>
2021-11-24 00:26:19 +0600

    github-graph added completely

--- index.js
+++ index.js
@@ -46,3 +46,19 @@ const focusFilterButton = (targetButton) => {
 // active-filter
 const activeOnMount = document.getElementById("activeOnMount");
 activeOnMount.click();
+
+// github gif adder
+var gifContainer = document.createElement('div');
+var scrollMsgTag = document.createElement('span');
+var gif = document.createElement('img');
+var scrollMsg = document.createTextNode("Slide Right");
+// gif.className.add('customAddedGif');
+//  gif.width="60px";
+gif.src = 'https://media3.giphy.com/media/JtBidqgB4MPjvix9MH/giphy.gif?cid=790b76113322cc1e49d868032acbf42a6dd96d05e9440b96&rid=giphy.gif&ct=s';
+// document.querySelector('.js-calendar-graph').appendChild(gif);
+// down.innerHTML = "Image Element Added."; 
+
+scrollMsgTag.appendChild(scrollMsg);
+gifContainer.appendChild(gif);
+gifContainer.appendChild(scrollMsgTag);
+document.querySelector('.js-calendar-graph').appendChild(gifContainer);
