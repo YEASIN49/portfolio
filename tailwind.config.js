b973ae08244d01e195e7db5aaf7ab954ab209c26
yeasin49 <myeasin49@gmail.com>
2021-12-04 23:07:10 +0600

    project card fixed. Review for alpha test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: ['./index.html','./index.js','./src_css/tailwind_src.css']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       backgroundImage: {
         'parallex-img': "url('../images/parallex-bg-3.jpg')",
-      }
+       }
     },
   },
   variants: {
